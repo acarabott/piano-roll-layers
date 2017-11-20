eb9ff49e882fdf49fa632f0374261ec6e388a65b
Arthur Carabott <arthurc@gmail.com>
2017-11-20 14:48:10 +0000

    fix up playback
    - play / stop button
    - AudioPlayback in own file

---
+++ js/AudioPlayback.js
@@ -0,0 +1,115 @@
+import { Note } from './Note.js';
+
+export class AudioPlayback {
+  constructor(audioContext) {
+    this.audio = audioContext;
+    this.lookahead = 0.5;
+    this.audioStart = 0;
+    this.notes = [];
+    this.marker = Symbol('played');
+    this._previewNote = undefined;
+    this._currentNodes = undefined;
+    this._nodes = new Map();
+    this.playing = false;
+  }
+
+  playFrom(notes) {
+    this.audioStart = this.audio.currentTime + this.lookahead;
+    this.notes = notes.map(note => note.clone());
+    this.playing = true;
+  }
+
+  stopNode(nodeObj) {
+    const gainNode = nodeObj.gain;
+    gainNode.gain.setTargetAtTime(0.0, this.audio.currentTime + 0.1, 0.001);
+    gainNode.gain.cancelScheduledValues(this.audio.currentTime + 0.2);
+    const oscNodes = nodeObj.oscs;
+    setTimeout(() => {
+      oscNodes.forEach(osc => osc.disconnect());
+      gainNode.disconnect();
+    }, 500);
+  }
+
+  stop() {
+    this._nodes.forEach((nodeObj, key) => {
+      this.stopNode(nodeObj);
+    });
+    this._nodes.clear();
+    this.notes = [];
+    this.playing = false;
+  }
+
+  playNote(note, audioStart) {
+    const gain = this.audio.createGain();
+    const oscs = [-5, 0, 5].map(detune => {
+      const osc = this.audio.createOscillator();
+      osc.frequency.value = note.freq;
+      osc.detune.value = detune;
+      osc.type = 'sine';
+      osc.connect(gain);
+      return osc;
+    });
+
+    const volume = 0.1;
+    gain.gain.setValueAtTime(0.0, this.audio.currentTime + 0.01);
+    gain.gain.setTargetAtTime(volume, audioStart + note.timeStart, 0.001);
+
+    const releaseTime = audioStart + note.timeStop - 0.1;
+    gain.gain.setValueAtTime(volume, releaseTime);
+    gain.gain.setTargetAtTime(0.0, releaseTime, 0.001);
+
+
+    gain.connect(this.audio.destination);
+
+    oscs.forEach(osc => {
+      osc.start(audioStart + note.timeStart);
+      osc.stop(audioStart + note.timeStop + 1);
+    });
+
+    return { note, oscs, gain };
+  }
+
+  update() {
+    if (!this.playing) { return; }
+
+    const toPlay = this.notes.filter(note => {
+      const noteStartTime = this.audioStart + note.timeStart;
+      return noteStartTime >= this.audio.currentTime &&
+             noteStartTime <= this.audio.currentTime + this.lookahead;
+    });
+
+    toPlay.forEach(note => {
+      if (!note[this.marker]) {
+        const nodes = this.playNote(note, this.audioStart);
+        this._nodes.set(note, nodes);
+        note[this.marker] = true;
+      }
+    });
+
+  }
+
+  set previewNote(note) {
+    const sameFreq = this._previewNote !== undefined &&
+                     note !== undefined &&
+                     Math.abs(this._previewNote.freq - note.freq) < Number.EPSILON;
+    if (sameFreq) { return; }
+
+    if (this._currentNodes !== undefined) {
+      const gainNode = this._currentNodes.gain;
+      gainNode.gain.setTargetAtTime(0.0, this.audio.currentTime + 0.1, 0.001);
+      gainNode.gain.cancelScheduledValues(this.audio.currentTime + 0.2);
+      const oscNodes = this._currentNodes.oscs;
+      setTimeout(() => {
+        oscNodes.forEach(osc => osc.disconnect());
+        gainNode.disconnect();
+      }, 500);
+      this._currentNodes = undefined;
+    }
+
+    this._previewNote = note === undefined ? undefined : new Note(note.freq, 0, 60);
+    // play new note
+    if (this._previewNote !== undefined) {
+      this._currentNodes = this.playNote(this._previewNote, this.audio.currentTime + 0.05);
+    }
+  }
+}
--- js/grid-layers.js
+++ js/grid-layers.js
@@ -1,6 +1,9 @@
 /*
   TODO:
-  - play note on draw
+  - moving playhead
+  - play from time
+  - style
+  - note release
   - instructions
   - put at top and overlay the vertical lines?
 */
@@ -12,19 +15,19 @@ import { loop, rrandint, midiToFreq } from './utils.js';
 import * as color from './color.js';
 import { ModeManager, ModeManagerRenderer } from './ModeManager.js';
 import { Note, NoteManager, NoteController, NoteRenderer } from './Note.js';
+import { AudioPlayback } from './AudioPlayback.js';
 import { LayerManager } from './LayerManager.js';
 import { Rectangle } from './Rectangle.js';
 import { Cursor } from './Cursor.js';
 import { Point } from './Point.js';
 import { Scroll } from './Scroll.js';
 
-const modeManager = new ModeManager();
-modeManager.addModes('layers', 'notes');
-modeManager.currentMode = modeManager.modes.layers;
-const modeManagerRenderer = new ModeManagerRenderer(modeManager);
+// constants
+const NUM_KEYS = 20;
+const DURATION = 10;
+const ROOT_NOTE = 60;
 
-const audio = new AudioContext();
-
+// canvas
 const canvas = document.createElement('canvas');
 canvas.width = 800;
 canvas.height = 400;
@@ -32,31 +35,18 @@ canvas.setAttribute('tabindex', 1);
 document.body.appendChild(canvas);
 const ctx = canvas.getContext('2d');
 
-const layerManager = new LayerManager();
-let snapping = true;
-const NUM_KEYS = 20;
-const DURATION = 10;
-const ROOT_NOTE = 60;
-
 const keyRect = new Rectangle(0, 0, canvas.width * 0.075, canvas.height);
 const patternRect = new Rectangle(keyRect.br.x,
                                   keyRect.y,
                                   canvas.width - keyRect.width,
                                   keyRect.height);
 
-const controls = document.createElement('div');
-controls.id = 'controls';
-document.body.appendChild(controls);
-const subdivisionInput = document.createElement('input');
-subdivisionInput.id = 'subdivision';
-subdivisionInput.type = 'text';
-subdivisionInput.value = layerManager.subdivision;
-const subdivisionLabel = document.createElement('label');
-subdivisionLabel.htmlFor = 'subdivision';
-subdivisionLabel.textContent = 'Subdivision: ';
-controls.appendChild(subdivisionLabel);
-controls.appendChild(subdivisionInput);
-controls.appendChild(modeManagerRenderer.label);
+// managers
+const modeManager = new ModeManager();
+modeManager.addModes('layers', 'notes');
+modeManager.currentMode = modeManager.modes.layers;
+const modeManagerRenderer = new ModeManagerRenderer(modeManager);
+const layerManager = new LayerManager();
 
 const noteManager = new NoteManager();
 const noteRenderer = new NoteRenderer();
@@ -65,6 +55,50 @@ noteRenderer.duration = DURATION;
 noteRenderer.numKeys = NUM_KEYS;
 noteRenderer.rootNote = ROOT_NOTE;
 const noteController = new NoteController(noteManager, noteRenderer);
+
+// audio
+const audio = new AudioContext();
+const audioPlayback = new AudioPlayback(audio);
+function startPlayback() {
+  audioPlayback.playFrom(noteManager.notes);
+}
+
+function stopPlayback() {
+  audioPlayback.stop();
+}
+
+
+
+
+// ui
+const controls = document.createElement('div');
+controls.id = 'controls';
+document.body.appendChild(controls);
+
+const playButton = document.createElement('button');
+playButton.textContent = 'Play';
+playButton.style.display = 'block';
+playButton.addEventListener('click', event => {
+  const play = playButton.textContent === 'Play';
+  play ? startPlayback() : stopPlayback();
+  playButton.textContent = play ? 'Stop' : 'Play';
+});
+controls.appendChild(playButton);
+
+const subdivisionInput = document.createElement('input');
+subdivisionInput.id = 'subdivision';
+subdivisionInput.type = 'text';
+subdivisionInput.value = layerManager.subdivision;
+
+const subdivisionLabel = document.createElement('label');
+subdivisionLabel.htmlFor = 'subdivision';
+subdivisionLabel.textContent = 'Subdivision: ';
+controls.appendChild(subdivisionLabel);
+controls.appendChild(subdivisionInput);
+controls.appendChild(modeManagerRenderer.label);
+
+// user input
+let snapping = true;
 
 const cursor = new Cursor();
 cursor.addCursorState(() => modeManager.currentMode === modeManager.modes.layers, 'crosshair');
@@ -75,101 +109,9 @@ cursor.addCursorState(() => modeManager.currentMode === modeManager.modes.notes,
 cursor.addCursorState(() => modeManager.currentMode === modeManager.modes.notes && noteController.isGrabbing, 'move');
 cursor.addCursorState(() => modeManager.currentMode === modeManager.modes.notes && noteController.isHovering, 'move');
 
-
-
 const scroll = new Scroll();
 scroll.range = 0.25;
 scroll.min = 1;
-
-
-class AudioPlayback {
-  constructor(audioContext) {
-    this.audio = audioContext;
-    this.lookahead = 0.5;
-    this.audioStart = 0;
-    this.notes = [];
-    this.marker = Symbol('played');
-    this._previewNote = undefined;
-    this._currentNodes = undefined;
-  }
-
-  playFrom(notes) {
-    this.audioStart = this.audio.currentTime + this.lookahead;
-    this.notes = notes.map(note => note.clone());
-  }
-
-  playNote(note, audioStart) {
-    const gain = this.audio.createGain();
-    const oscs = [-5, 0, 5].map(detune => {
-      const osc = this.audio.createOscillator();
-      osc.frequency.value = note.freq;
-      osc.detune.value = detune;
-      osc.type = 'sine';
-      osc.connect(gain);
-      return osc;
-    });
-
-    const volume = 0.1;
-    gain.gain.setValueAtTime(0.0, this.audio.currentTime + 0.01);
-    gain.gain.setTargetAtTime(volume, audioStart + note.timeStart, 0.001);
-
-    const releaseTime = audioStart + note.timeStop - 0.1;
-    gain.gain.setValueAtTime(volume, releaseTime);
-    gain.gain.setTargetAtTime(0.0, releaseTime, 0.001);
-
-
-    gain.connect(this.audio.destination);
-
-    oscs.forEach(osc => {
-      osc.start(audioStart + note.timeStart);
-      osc.stop(audioStart + note.timeStop + 1);
-    });
-
-    return { note, oscs, gain };
-  }
-
-  update() {
-    const toPlay = this.notes.filter(note => {
-      const noteStartTime = this.audioStart + note.timeStart;
-      return noteStartTime >= this.audio.currentTime &&
-             noteStartTime <= this.audio.currentTime + this.lookahead;
-    });
-
-    toPlay.forEach(note => {
-      if (!note[this.marker]) {
-        this.playNote(note, this.audioStart);
-        note[this.marker] = true;
-      }
-    });
-  }
-
-  set previewNote(note) {
-    const sameFreq = this._previewNote !== undefined &&
-                     note !== undefined &&
-                     Math.abs(this._previewNote.freq - note.freq) < Number.EPSILON;
-    if (sameFreq) { return; }
-
-    if (this._currentNodes !== undefined) {
-      const gainNode = this._currentNodes.gain;
-      gainNode.gain.setTargetAtTime(0.0, this.audio.currentTime + 0.1, 0.001);
-      gainNode.gain.cancelScheduledValues(this.audio.currentTime + 0.2);
-      const oscNodes = this._currentNodes.oscs;
-      setTimeout(() => {
-        oscNodes.forEach(osc => osc.disconnect());
-        gainNode.disconnect();
-      }, 500);
-      this._currentNodes = undefined;
-    }
-
-    this._previewNote = note === undefined ? undefined : new Note(note.freq, 0, 60);
-    // play new note
-    if (this._previewNote !== undefined) {
-      this._currentNodes = this.playNote(this._previewNote, this.audio.currentTime + 0.05);
-    }
-  }
-}
-
-const audioPlayback = new AudioPlayback(audio);
 
 // Render functions
 // -----------------------------------------------------------------------------
