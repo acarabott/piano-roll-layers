d78d11dba3c529baa1aa76c9c3e8680751affb07
Arthur Carabott <arthurc@gmail.com>
2017-11-28 14:34:50 +0000

    generating html with stripped elements

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const buildPath = 'dist';
 
 module.exports = {
   entry: ['babel-polyfill', './js/grid-layers.js'],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, buildPath)
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+    }),
+    new CleanWebpackPlugin([buildPath]),
+  ],
+
   module: {
     rules: [
       {
@@ -17,6 +28,30 @@ module.exports = {
             // presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        enforce: 'pre',
+        use: [
+          {
+            loader: 'html-loader'
+          },
+          {
+            loader: 'webpack-strip-blocks',
+            options: {
+                blocks: ['REMOVE'],
+                start: '<!--',
+                end: '-->'
+            }
+          }
+        ]
       }
     ]
   }
